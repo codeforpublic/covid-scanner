db1c2195328982caa8a99393b2514bb8b3989b0c
Pinyaporn Eiammongkol <pinyapornster@gmail.com>
2020-03-17 02:19:55 +0700

    ➖ remove styled

--- pages/_document.js
+++ pages/_document.js
@@ -15,7 +15,7 @@ class MyDocument extends Document {
             href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"
           />
           <link
-            href="https://fonts.googleapis.com/css?family=Prompt:300,400,600&display=swap&subset=thai"
+            href="https://fonts.googleapis.com/css?family=Prompt:300,600&display=swap&subset=thai"
             rel="stylesheet"
           />
           <link
