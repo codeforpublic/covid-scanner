b8d303cf59f3df5fdbe38c00393913bcd0e5df73
Aikdanai Sidhikosol <aikdanai.s@gmail.com>
2020-03-16 23:06:49 +0700

    ✨ decode jwt

--- src/Result.js
+++ src/Result.js
@@ -1,4 +1,11 @@
+import { useMemo } from 'react'
+import { decodeJWT } from './utils/jwt'
+
 export const Result = ({ result, onRescan }) => {
+  const decoded = useMemo(() => {
+    decodeJWT(result)
+  }, [result])
+  console.log('decoded -> decoded', decoded)
   return (
     <div>
       {result.toString()}
